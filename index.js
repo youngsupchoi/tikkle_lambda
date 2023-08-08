daaad8027ec735ab55f466a8fc739ae7d981628c
youngsupchoi <sup1214@naver.com>
2023-08-08 17:58:06 +0900

    Fix: 충돌 해결

--- index.js
+++ index.js
@@ -6,109 +6,109 @@ const { authtoken } = require("authtoken.js");
 
 //auth
 const {
-	get_auth_checkToken,
+  get_auth_checkToken,
 } = require("./tikkle_auth/get_auth_checkToken/index.js");
 const {
-	post_auth_IdDuplicationCheck,
+  post_auth_IdDuplicationCheck,
 } = require("./tikkle_auth/post_auth_IdDuplicationCheck/index.js");
 const {
-	post_auth_phoneCheck,
+  post_auth_phoneCheck,
 } = require("./tikkle_auth/post_auth_phoneCheck/index.js");
 const {
-	post_auth_registerUser,
+  post_auth_registerUser,
 } = require("./tikkle_auth/post_auth_registerUser/index.js");
 const {
-	post_auth_tokenGenerate,
+  post_auth_tokenGenerate,
 } = require("./tikkle_auth/post_auth_tokenGenerate/index.js");
 
 //friend
 const { get_friend_data } = require("./tikkle_friend/get_friend_data/index.js");
 const {
-	get_friend_event,
+  get_friend_event,
 } = require("./tikkle_friend/get_friend_event/index.js");
 const {
-	get_friend_search,
+  get_friend_search,
 } = require("./tikkle_friend/get_friend_search/index.js");
 const {
-	post_friend_phonecheck,
+  post_friend_phonecheck,
 } = require("./tikkle_friend/post_friend_phonecheck/index.js");
 const {
-	put_friend_block,
+  put_friend_block,
 } = require("./tikkle_friend/put_friend_block/index.js");
 
 //image
 const {
-	get_image_deleteProfile,
+  get_image_deleteProfile,
 } = require("./tikkle_image/get_image_deleteProfile/index.js");
 const {
-	get_image_profileSaveUrl,
+  get_image_profileSaveUrl,
 } = require("./tikkle_image/get_image_profileSaveUrl/index.js");
 const {
-	post_image_profileUrl,
+  post_image_profileUrl,
 } = require("./tikkle_image/post_image_profileUrl/index.js");
 
 //notification
 const {
-	get_notification_list,
+  get_notification_list,
 } = require("./tikkle_notification/get_notification_list/index.js");
 const {
-	post_notification_send,
+  post_notification_send,
 } = require("./tikkle_notification/post_notification_send/index.js");
 const {
-	put_notification_delete,
+  put_notification_delete,
 } = require("./tikkle_notification/put_notification_delete/index.js");
 
 //post
 const {
-	post_product_images,
+  post_product_images,
 } = require("./tikkle_product/post_product_images/index.js");
 const {
-	post_product_info,
+  post_product_info,
 } = require("./tikkle_product/post_product_info/index.js");
 const {
-	post_product_list,
+  post_product_list,
 } = require("./tikkle_product/post_product_list/index.js");
 const {
-	put_product_viewIncrease,
+  put_product_viewIncrease,
 } = require("./tikkle_product/put_product_viewIncrease/index.js");
 
 //tikkling
 const {
-	get_tikkling_friendinfo,
+  get_tikkling_friendinfo,
 } = require("./tikkle_tikkling/get_tikkling_friendinfo/index.js");
 const {
-	get_tikkling_info,
+  get_tikkling_info,
 } = require("./tikkle_tikkling/get_tikkling_info/index.js");
 const {
-	get_tikkling_recivedTikkle,
+  get_tikkling_recivedTikkle,
 } = require("./tikkle_tikkling/get_tikkling_recivedTikkle/index.js");
 const {
-	post_tikkling_create,
+  post_tikkling_create,
 } = require("./tikkle_tikkling/post_tikkling_create/index.js");
 const {
-	put_tikkling_end,
+  put_tikkling_end,
 } = require("./tikkle_tikkling/put_tikkling_end/index.js");
 
 //user
 const {
-	delete_user_wishlist,
+  delete_user_wishlist,
 } = require("./tikkle_user/delete_user_wishlist/index.js");
 const {
-	get_user_checkTikkling,
+  get_user_checkTikkling,
 } = require("./tikkle_user/get_user_checkTikkling/index.js");
 const {
-	get_user_endTikklings,
+  get_user_endTikklings,
 } = require("./tikkle_user/get_user_endTikklings/index.js");
 const { get_user_info } = require("./tikkle_user/get_user_info/index.js");
 const {
-	get_user_myWishlist,
+  get_user_myWishlist,
 } = require("./tikkle_user/get_user_myWishlist/index.js");
 const {
-	get_user_paymentHistory,
+  get_user_paymentHistory,
 } = require("./tikkle_user/get_user_paymentHistory/index.js");
 const { post_user_friend } = require("./tikkle_user/post_user_friend/index.js");
 const {
-	post_user_wishlist,
+  post_user_wishlist,
 } = require("./tikkle_user/post_user_wishlist/index.js");
 
 //-------- API's ------------------------------------------------//
@@ -127,6 +127,7 @@ api.post("/post_auth_tokenGenerate", post_auth_tokenGenerate);
 //
 
 //------- friend
+
 api.get("/get_friend_data", get_friend_data);
 
 api.get("/get_friend_event", get_friend_event);
@@ -140,22 +141,20 @@ api.put("/put_friend_block", put_friend_block);
 //
 
 //------- image
+
 api.get("/get_image_deleteProfile", authtoken, get_image_deleteProfile);
 
 api.get("/get_image_profileSaveUrl", authtoken, get_image_profileSaveUrl);
 
 api.post("/post_image_profileUrl", authtoken, post_image_profileUrl);
 
-//
-
 //------- notification
+
 api.get("/get_notification_list", authtoken, get_notification_list);
 
 api.post("/post_notification_send", authtoken, post_notification_send);
 
 api.put("/put_notification_delete", authtoken, put_notification_delete);
-
-//
 
 //------- product
 api.post("/post_product_images", authtoken, post_product_images);
@@ -203,5 +202,5 @@ api.post("/post_user_wishlist", authtoken, post_user_wishlist);
 //-------- handler ------------------------------------------------//
 
 exports.handler = async (req, context) => {
-	return await api.run(req, context);
+  return await api.run(req, context);
 };
--- tikkle_friend/get_friend_data/index.js
+++ tikkle_friend/get_friend_data/index.js
@@ -1,92 +1,95 @@
 const { queryDatabase } = require("db.js");
 const { checkToken } = require("token.js");
 exports.get_friend_data = async (req, res) => {
-	const headers = req.headers;
-	const body = req.body;
-	const authorization = headers.authorization;
-	const [accessToken, refreshToken] = authorization.split(",");
+  const headers = req.headers;
+  const body = req.body;
+  const authorization = headers.authorization;
+  const [accessToken, refreshToken] = authorization.split(",");
 
-	//-------- check token & get user id --------------------------------------------------------------------------------------//
+  //-------- check token & get user id --------------------------------------------------------------------------------------//
 
-	let tokenCheck;
-	let returnBody;
-	let id;
+  let tokenCheck;
+  let returnBody;
+  let id;
+  console.log("-------------------------------------------------");
+  console.log("hihi:", event.params.mode);
+  console.log("-------------------------------------------------");
 
-	try {
-		tokenCheck = await checkToken(accessToken, refreshToken);
-		returnBody = JSON.parse(tokenCheck.body);
-		id = returnBody.tokenData.id;
-	} catch (error) {
-		//return invalid when token is invalid
-		console.log("ERROR : the token value is null or invalid");
-		return {
-			statusCode: 410,
-			body: "login again",
-		};
-	}
+  try {
+    tokenCheck = await checkToken(accessToken, refreshToken);
+    returnBody = JSON.parse(tokenCheck.body);
+    id = returnBody.tokenData.id;
+  } catch (error) {
+    //return invalid when token is invalid
+    console.log("ERROR : the token value is null or invalid");
+    return {
+      statusCode: 410,
+      body: "login again",
+    };
+  }
 
-	//return invalid when token is invalid
-	if (tokenCheck.statusCode !== 200) {
-		console.log("ERROR : the token value is null or invalid");
-		return {
-			statusCode: 410,
-			body: "login again",
-		};
-	}
+  //return invalid when token is invalid
+  if (tokenCheck.statusCode !== 200) {
+    console.log("ERROR : the token value is null or invalid");
+    return {
+      statusCode: 410,
+      body: "login again",
+    };
+  }
 
-	const returnToken = returnBody.accessToken;
+  const returnToken = returnBody.accessToken;
 
-	try {
-		//차단된 친구 목록
-		let rows;
-		if (req.queryStrignParameters.mode === "block") {
-			rows = await queryDatabase(
-				"SELECT u.name, u.image, u.nick FROM users u INNER JOIN friends_relation fr ON u.id = fr.friend_user_id WHERE fr.relation_state_id = 3 AND fr.central_user_id = ?",
-				[id]
-			);
-			//차단되지 않은 친구 목록
-		} else if (req.queryStrignParameters.mode === "unblock") {
-			rows = await queryDatabase(
-				"SELECT u.name, u.image, u.nick FROM users u INNER JOIN friends_relation fr ON u.id = fr.friend_user_id WHERE fr.relation_state_id != 3 AND fr.central_user_id = ?",
-				[id]
-			);
+  try {
+    //차단된 친구 목록
+    let rows;
+    if (req.queryStrignParameters.mode === "block") {
+      rows = await queryDatabase(
+        "SELECT u.name, u.image, u.nick FROM users u INNER JOIN friends_relation fr ON u.id = fr.friend_user_id WHERE fr.relation_state_id = 3 AND fr.central_user_id = ?",
+        [id]
+      );
+      //차단되지 않은 친구 목록
+    } else if (req.queryStrignParameters.mode === "unblock") {
+      rows = await queryDatabase(
+        "SELECT u.name, u.image, u.nick FROM users u INNER JOIN friends_relation fr ON u.id = fr.friend_user_id WHERE fr.relation_state_id != 3 AND fr.central_user_id = ?",
+        [id]
+      );
 
-			//확인한 친구에 대해 모두 친구로 전환
-			await queryDatabase(
-				"UPDATE friends_relation SET relation_state_id = 1 WHERE relation_state_id = 2 AND central_user_id = ?",
-				[id]
-			);
-		} else {
-			// parameter잘못된 mode를 전송
-			const return_body = {
-				success: false,
-				data: "잘못된 mode",
-			};
-			console.log("잘못된 mode");
-			return {
-				statusCode: 400,
-				body: JSON.stringify(return_body),
-			};
-		}
+      //확인한 친구에 대해 모두 친구로 전환
+      await queryDatabase(
+        "UPDATE friends_relation SET relation_state_id = 1 WHERE relation_state_id = 2 AND central_user_id = ?",
+        [id]
+      );
+    } else {
+      // parameter잘못된 mode를 전송
+      const return_body = {
+        success: false,
+        data: "잘못된 mode",
+      };
+      console.log("잘못된 mode");
+      return {
+        statusCode: 400,
+        body: JSON.stringify(return_body),
+      };
+    }
 
-		const return_body = {
-			success: true,
-			data: rows,
-			returnToken,
-		};
-		return {
-			statusCode: 200,
-			body: JSON.stringify(return_body),
-		};
-	} catch (err) {
-		console.error("Failed to connect or execute query:", err);
-		const return_body = {
-			success: false,
-			data: err,
-		};
-		return {
-			statusCode: 500,
-			body: JSON.stringify(return_body),
-		};
-	}
+    const return_body = {
+      success: true,
+      data: rows,
+      returnToken,
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(return_body),
+    };
+  } catch (err) {
+    console.error("Failed to connect or execute query:", err);
+    const return_body = {
+      success: false,
+      data: err,
+    };
+    return {
+      statusCode: 500,
+      body: JSON.stringify(return_body),
+    };
+  }
 };
