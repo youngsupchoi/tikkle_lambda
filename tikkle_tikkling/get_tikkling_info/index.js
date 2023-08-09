52bc13dce383add574f36e05a268a0593f6f313e
youngsupchoi <sup1214@naver.com>
2023-08-09 10:43:18 +0900

    Fix: get_tikkling_info에 데이터 추가

--- tikkle_tikkling/get_tikkling_info/index.js
+++ tikkle_tikkling/get_tikkling_info/index.js
@@ -1,5 +1,4 @@
 const { queryDatabase } = require("db.js");
-const { checkToken } = require("token.js");
 
 exports.get_tikkling_info = async (req, res) => {
   const body = req.body;
@@ -13,35 +12,45 @@ exports.get_tikkling_info = async (req, res) => {
     const tikkling_id = req.params ? req.params.tikkling_id : null;
 
     if (!tikkling_id) {
-      throw new Error("입력 오류: tikkling_id 파라미터가 필요합니다.");
-    }
+      //tikkling_id 파라미터가 없을 경우 자신의 tikkling 정보를 DB에서 조회
+      const query = `SELECT users.id as user_id, users.name as user_name, view.id as tikkling_id, view.funding_limit, view.tikkle_quantity, view.tikkle_count, view.thumbnail_image, view.brand_name, view.name as product_name  FROM active_tikkling_view as view inner join users on view.user_id = users.id WHERE users.id = ?`;
+      const rows = await queryDatabase(query, [id]);
+      if (rows.length === 0) {
+        return res
+          .status(404)
+          .send({ success: false, message: "티클링 정보가 없습니다." });
+      }
+      const return_body = {
+        success: true,
+        data: rows,
+        message: "나의 티클링 정보 조회 성공",
+      };
+      return res.status(200).send(return_body);
+    } else {
+      const parsedId = parseInt(tikkling_id, 10);
 
-    const parsedId = parseInt(tikkling_id, 10);
+      if (isNaN(parsedId)) {
+        throw new Error("입력 오류: tikkling_id는 숫자여야 합니다.");
+      }
 
-    if (isNaN(parsedId)) {
-      throw new Error("입력 오류: tikkling_id는 숫자여야 합니다.");
+      // tikkling_id와 일치하는 tikkling의 정보를 DB에서 조회(내가 아닌 유저)
+      const query = `SELECT users.id as user_id, users.name as user_name, view.id as tikkling_id, view.funding_limit, view.tikkle_quantity, view.tikkle_count, view.thumbnail_image, view.brand_name, view.name as product_name  FROM active_tikkling_view as view inner join users on view.user_id = users.id WHERE view.id = ?`;
+      const rows = await queryDatabase(query, [parsedId]);
+      if (rows.length === 0) {
+        return res
+          .status(404)
+          .send({ success: false, message: "티클링 정보가 없습니다." });
+      }
+      const return_body = {
+        success: true,
+        data: rows,
+        message: `$tikkling_id = {tikkling_id}의 티클링 정보 조회 성공`,
+      };
+      return res.status(200).send(return_body);
     }
-
-    // tikkling_id와 일치하는 tikkling의 정보를 DB에서 조회
-    const query = `SELECT * FROM tikkling WHERE id = ?`;
-    const rows = await queryDatabase(query, [parsedId]);
-    if (rows.length === 0) {
-      return res
-        .status(404)
-        .send({ success: false, message: "티클링 정보가 없습니다." });
-    }
-    const return_body = {
-      success: true,
-      data: rows,
-      message: "티클링 정보 조회 성공",
-    };
-    return res.status(200).send(return_body);
   } catch (error) {
     console.log("에러 : ", error);
-    if (
-      error.message === "입력 오류: tikkling_id 파라미터가 필요합니다." ||
-      error.message === "입력 오류: tikkling_id는 숫자여야 합니다."
-    ) {
+    if (error.message === "입력 오류: tikkling_id는 숫자여야 합니다.") {
       return {
         statusCode: 400,
         body: JSON.stringify({
