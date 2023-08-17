2b63f76ccb0169d1301f2f1fe498366d8735d68f
sjsjmine129 <sjsjmine129@gmail.com>
2023-08-17 11:35:42 +0900

    Add/ put_user_address

---
+++ tikkle_user/put_user_address/index.js
@@ -0,0 +1,69 @@
+const { queryDatabase } = require("db.js");
+
+exports.put_user_address = async (req, res) => {
+	const body = req.body;
+	const id = req.id;
+	const returnToken = req.returnToken;
+
+	const zonecode = body.zonecode;
+	const address = body.address;
+	const detail_address = body.detail_address;
+
+	//-------- check address --------------------------------------------------------------------------------------//
+
+	//check productId
+	if (
+		!zonecode ||
+		!address ||
+		!detail_address ||
+		typeof zonecode !== "string" ||
+		typeof address !== "string" ||
+		typeof detail_address !== "string" ||
+		zonecode.length !== 5 ||
+		address.length > 250 ||
+		detail_address.length > 250
+	) {
+		console.log("put_user_addressd의 입력 데이터에서 에러가 발생했습니다.");
+		const return_body = {
+			success: false,
+			data: null,
+			message: "address value is null or invalid",
+		};
+		return res.status(401).send(return_body);
+	}
+
+	//--------  update address  --------------------------------------------------------------------------------------//
+
+	let sqlResult;
+
+	try {
+		const rows = await queryDatabase(
+			`	UPDATE users
+				SET	zonecode = ?,	address = ?, detail_address = ?
+				WHERE	id = ?
+			`,
+			[zonecode, address, detail_address, id]
+		);
+
+		sqlResult = rows;
+		//console.log("SQL result : ", sqlResult);
+	} catch (err) {
+		console.log(" put_user_address의 query에서 에러가 발생했습니다.", err);
+		const return_body = {
+			success: false,
+			data: null,
+			message: "SQL error",
+		};
+		return res.status(501).send(return_body);
+	}
+
+	//-------- return result --------------------------------------------------------------------------------------//
+
+	const return_body = {
+		success: true,
+		data: sqlResult,
+		message: "success to update address",
+		returnToken,
+	};
+	return res.status(200).send(return_body);
+};
