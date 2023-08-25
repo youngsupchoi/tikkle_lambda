2fd58b1d23ceaa6a306b5cd74f62e0f972dcd8f2
youngsupchoi <sup1214@naver.com>
2023-08-25 14:35:52 +0900

    Fix: tikkling_receivedTikkle return body

--- tikkle_tikkling/post_tikkling_receivedTikkle/index.js
+++ tikkle_tikkling/post_tikkling_receivedTikkle/index.js
@@ -1,41 +1,52 @@
 const { queryDatabase } = require("db.js");
 
 exports.post_tikkling_receivedTikkle = async (req, res) => {
-	const body = req.body;
-	const id = req.id;
-	const returnToken = req.returnToken;
+  const body = req.body;
+  const id = req.id;
+  const returnToken = req.returnToken;
 
-	//main logic------------------------------------------------------------------------------------------------------------------//
+  //main logic------------------------------------------------------------------------------------------------------------------//
 
-	try {
-		const rows = await queryDatabase(
-			`SELECT sending_tikkle.*,
+  try {
+    const tikkling_id = req.body ? req.body.tikkling_id : null;
+    const parsedId = parseInt(tikkling_id, 10);
+    if (isNaN(parsedId)) {
+      const return_body = {
+        success: false,
+        detail_code: "00",
+        message: "잘못된 요청, tikkling_id는 숫자여야 합니다.",
+        returnToken: null,
+      };
+      return res.status(400).send(return_body);
+    }
+    const rows = await queryDatabase(
+      `SELECT sending_tikkle.*,
 			users.id,
 			users.NAME,
 			users.image
 			FROM   sending_tikkle
 			INNER JOIN users ON sending_tikkle.user_id = users.id
 			WHERE  sending_tikkle.tikkling_id = ?; `,
-			[body.tikkling_id]
-		);
+      [body.tikkling_id]
+    );
 
-		const return_body = {
-			success: true,
-			data: rows,
-			message_title: null,
-			message_detail: null,
-			message: "특정 티클링의 받은 티클 정보 조회 성공",
-			returnToken,
-		};
-		return res.status(200).send(return_body);
-	} catch (err) {
-		console.error("Failed to connect or execute query:", err);
-		const return_body = {
-			success: false,
-			message_title: null,
-			message_detail: null,
-			message: "서버 에러",
-		};
-		return res.status(500).send(return_body);
-	}
+    const return_body = {
+      success: true,
+      data: rows,
+      message_title: null,
+      message_detail: null,
+      message: "특정 티클링의 받은 티클 정보 조회 성공",
+      returnToken,
+    };
+    return res.status(200).send(return_body);
+  } catch (err) {
+    console.error("Failed to connect or execute query:", err);
+    const return_body = {
+      success: false,
+      message_title: null,
+      message_detail: null,
+      message: "서버 에러",
+    };
+    return res.status(500).send(return_body);
+  }
 };
