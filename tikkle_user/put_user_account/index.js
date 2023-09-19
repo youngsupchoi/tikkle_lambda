d80361a94552133ee6bd77b01f553dd4ba741b5e
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-19 11:26:28 +0900

    Fix/ account_encrypt

--- tikkle_user/put_user_account/index.js
+++ tikkle_user/put_user_account/index.js
@@ -1,65 +1,84 @@
 const { queryDatabase } = require("db.js");
+const crypto = require("crypto");
+const { getSSMParameter } = require("ssm.js");
 
 exports.put_user_account = async (req, res) => {
-  const body = req.body;
-  const id = req.id;
-  const returnToken = req.returnToken;
+	const body = req.body;
+	const id = req.id;
+	const returnToken = req.returnToken;
 
-  const bank_name = body.bank_name;
-  const account = body.account;
+	const bank_name = body.bank_name;
+	const account = body.account;
 
-  //-------- check address --------------------------------------------------------------------------------------//
+	//-------- check account --------------------------------------------------------------------------------------//
 
-  //check productId
-  if (
-    !bank_name ||
-    !account ||
-    typeof bank_name !== "string" ||
-    typeof account !== "string"
-  ) {
-    console.log("put_user_addressd의 입력 데이터에서 에러가 발생했습니다.");
-    const return_body = {
-      success: false,
-      detail_code: "00",
-      message: "input value is null or invalid",
-      returnToken: null,
-    };
-    return res.status(400).send(return_body);
-  }
+	//check productId
+	if (
+		!bank_name ||
+		!account ||
+		typeof bank_name !== "string" ||
+		typeof account !== "string"
+	) {
+		console.log("put_user_account의 입력 데이터에서 에러가 발생했습니다.");
+		const return_body = {
+			success: false,
+			detail_code: "00",
+			message: "input value is null or invalid",
+			returnToken: null,
+		};
+		return res.status(400).send(return_body);
+	}
 
-  //--------  update address  --------------------------------------------------------------------------------------//
+	//-------- encrypt account --------------------------------------------------------------------------------------//
+	const algorithm = "aes-256-cbc"; // Use the same algorithm that was used for encryption
+	const accountkeyHex = await getSSMParameter("accountkeyHex");
+	const accountivHex = await getSSMParameter("accountivHex");
 
-  let sqlResult;
+	const key = Buffer.from(accountkeyHex, "hex");
+	const iv = Buffer.from(accountivHex, "hex");
+	const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-  try {
-    const rows = await queryDatabase(
-      `	UPDATE users
+	// console.log("key : ", key);
+	// console.log("iv : ", iv);
+
+	let encryptedAccount = cipher.update(account, "utf-8", "hex");
+	encryptedAccount += cipher.final("hex");
+
+	//console.log("Encrypted Data:", encryptedAccount);
+
+	//--------  update account  --------------------------------------------------------------------------------------//
+
+	let sqlResult;
+
+	try {
+		const rows = await queryDatabase(
+			`	UPDATE users
 				SET	bank_name = ?,	account = ?
 				WHERE	id = ?
 			`,
-      [bank_name, account, id]
-    );
+			[bank_name, encryptedAccount, id]
+		);
 
-    sqlResult = rows;
-    //console.log("SQL result : ", sqlResult);
-  } catch (err) {
-    console.log("put_user_address의 query에서 에러가 발생했습니다.", err);
-    const return_body = {
-      success: false,
-      detail_code: "00",
-      message: "SQL error",
-      returnToken: null,
-    };
-    return res.status(500).send(return_body);
-  }
+		sqlResult = rows;
+		//console.log("SQL result : ", sqlResult);
+	} catch (err) {
+		console.log("put_user_account의 query에서 에러가 발생했습니다.", err);
+		const return_body = {
+			success: false,
+			detail_code: "00",
+			message: "SQL error",
+			returnToken: null,
+		};
+		return res.status(500).send(return_body);
+	}
 
-  //-------- return result --------------------------------------------------------------------------------------//
+	//-------- return result --------------------------------------------------------------------------------------//
 
-  const return_body = {
-    success: true,
-    detail_code: "00",
-    message: "success to update account info",
-    returnToken: returnToken,
-  };
-  return res.status(200).send(return_body);
+	const return_body = {
+		success: true,
+		detail_code: "00",
+		message: "success to update account info",
+		returnToken: returnToken,
+	};
+	return res.status(200).send(return_body);
 };
