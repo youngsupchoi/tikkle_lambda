4322a1b8c702cbb1cd4291ba802d791457afa94d
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-26 13:32:10 +0900

    Add/delete user

--- tikkle_tikkling/get_tikkling_info/index.js
+++ tikkle_tikkling/get_tikkling_info/index.js
@@ -7,13 +7,9 @@ exports.get_tikkling_info = async (req, res) => {
 
 	//main logic------------------------------------------------------------------------------------------------------------------//
 
-	try {
-		// 쿼리 스트링 파라미터에서 tikkling_id를 추출, 숫자인지 확인
-		const tikkling_id = req.params ? req.params.tikkling_id : null;
-
-		if (tikkling_id == 0) {
-			//tikkling_id 파라미터가 없을 경우 자신의 tikkling 정보를 DB에서 조회
-			const query = `SELECT 
+	if (tikkling_id == 0) {
+		//tikkling_id 파라미터가 없을 경우 자신의 tikkling 정보를 DB에서 조회
+		const query = `SELECT 
       u.id AS user_id, 
       u.name AS user_name, 
       a.tikkling_id, 
@@ -31,32 +27,32 @@ exports.get_tikkling_info = async (req, res) => {
       JOIN users u ON a.user_id = u.id 
       JOIN product_category pc ON a.category_id = pc.id 
       WHERE u.id = ?;`;
-			const rows = await queryDatabase(query, [id]);
-			if (rows.length == 0) {
-				return res.status(404).send({
-					success: false,
-					detail_code: "00",
-					message: "티클링 정보가 없습니다.",
-					returnToken: null,
-				});
-			}
-			const return_body = {
-				success: true,
-				data: rows,
-				detail_code: "01",
-				message: "나의 티클링 정보 조회 성공",
-				returnToken,
-			};
-			return res.status(200).send(return_body);
-		} else {
-			const parsedId = parseInt(tikkling_id, 10);
+		const rows = await queryDatabase(query, [id]);
+		if (rows.length == 0) {
+			return res.status(404).send({
+				success: false,
+				detail_code: "00",
+				message: "티클링 정보가 없습니다.",
+				returnToken: null,
+			});
+		}
+		const return_body = {
+			success: true,
+			data: rows,
+			detail_code: "01",
+			message: "나의 티클링 정보 조회 성공",
+			returnToken,
+		};
+		return res.status(200).send(return_body);
+	} else {
+		const parsedId = parseInt(tikkling_id, 10);
 
-			if (isNaN(parsedId)) {
-				throw new Error("입력 오류: tikkling_id는 숫자여야 합니다.");
-			}
+		if (isNaN(parsedId)) {
+			throw new Error("입력 오류: tikkling_id는 숫자여야 합니다.");
+		}
 
-			// tikkling_id와 일치하는 tikkling의 정보를 DB에서 조회(내가 아닌 유저, state_id = 1만 조회 가능)
-			const query = `SELECT 
+		// tikkling_id와 일치하는 tikkling의 정보를 DB에서 조회(내가 아닌 유저, state_id = 1만 조회 가능)
+		const query = `SELECT 
       u.id AS user_id, 
       u.name AS user_name, 
       a.tikkling_id, 
@@ -75,43 +71,22 @@ exports.get_tikkling_info = async (req, res) => {
       JOIN product_category pc ON a.category_id = pc.id 
       WHERE a.id = ? AND a.state_id = 1;
       `;
-			const rows = await queryDatabase(query, [parsedId]);
-			if (rows.length == 0) {
-				return res.status(404).send({
-					success: false,
-					detail_code: "00",
-					message: "티클링 정보가 없습니다.",
-					returnToken: null,
-				});
-			}
-			const return_body = {
-				success: true,
-				data: rows,
-				detail_code: "02",
-				message: `tikkling_id = ${tikkling_id}의 티클링 정보 조회 성공`,
-				returnToken,
-			};
-			return res.status(200).send(return_body);
-		}
-	} catch (error) {
-		console.log("에러 : ", error);
-		if (error.message === "입력 오류: tikkling_id는 숫자여야 합니다.") {
-			const return_body = {
+		const rows = await queryDatabase(query, [parsedId]);
+		if (rows.length == 0) {
+			return res.status(404).send({
 				success: false,
 				detail_code: "00",
-				message: "tikkling_id는 숫자여야 합니다.",
-				returnToken,
-			};
-			return res.status(400).send(return_body);
-		} else {
-			console.log("get_tikkling_info에서 문제가 발생했습니다.");
-			const return_body = {
-				success: false,
-				detail_code: "00",
-				message: "서버 오류",
-				returnToken,
-			};
-			return res.status(500).send(return_body);
+				message: "티클링 정보가 없습니다.",
+				returnToken: null,
+			});
 		}
+		const return_body = {
+			success: true,
+			data: rows,
+			detail_code: "02",
+			message: `tikkling_id = ${tikkling_id}의 티클링 정보 조회 성공`,
+			returnToken,
+		};
+		return res.status(200).send(return_body);
 	}
 };
---
+++ tikkle_user/get_user_deleteUser/index.js
@@ -0,0 +1,46 @@
+const { queryDatabase } = require("db.js");
+const crypto = require("crypto");
+const { getSSMParameter } = require("ssm.js");
+
+exports.get_user_deleteUser = async (req, res) => {
+	const body = req.body;
+	const id = req.id;
+	const returnToken = req.returnToken;
+
+	//-------- change delete state in 1  --------------------------------------------------------------------------------------//
+
+	let sqlResult;
+
+	try {
+		const rows = await queryDatabase(
+			`	UPDATE users
+				SET	 is_deleted = ?
+				WHERE	id = ?
+			`,
+			[1, id]
+		);
+
+		sqlResult = rows;
+		//console.log("SQL result : ", sqlResult);
+	} catch (err) {
+		console.log("get_user_deleteUser의 query에서 에러가 발생했습니다.", err);
+		const return_body = {
+			success: false,
+			detail_code: "00",
+			message: "SQL error",
+			returnToken: null,
+		};
+		return res.status(500).send(return_body);
+	}
+
+	//-------- return result --------------------------------------------------------------------------------------//
+
+	const return_body = {
+		success: true,
+		data: null,
+		detail_code: "00",
+		message: "success to delete user",
+		returnToken: returnToken,
+	};
+	return res.status(200).send(return_body);
+};
