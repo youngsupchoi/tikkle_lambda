43d7d14a99f3a6e5d4ad3d63738c7e608e255599
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-27 16:30:19 +0900

    fix/resolution_type

--- tikkle_user/get_user_endTikklings/index.js
+++ tikkle_user/get_user_endTikklings/index.js
@@ -11,8 +11,8 @@ exports.get_user_endTikklings = async (req, res) => {
 
 	try {
 		const rows = await queryDatabase(
-			`SELECT tikkling.id AS tikkling_id, tikkling.user_id, tikkling.type AS tikkling_type, tikkling.funding_limit, tikkling.created_at, tikkling.tikkle_quantity, tikkling.terminated_at, tikkling.product_id, tikkling.terminated_at,
-			tikkling.state_id, tikkling_state.name AS state_name,
+			`SELECT tikkling.id AS tikkling_id, tikkling.user_id, tikkling.resolution_type AS resolution_type, tikkling.type AS tikkling_type, tikkling.funding_limit, tikkling.created_at, tikkling.tikkle_quantity, tikkling.terminated_at, tikkling.product_id, tikkling.terminated_at,
+			tikkling.state_id, tikkling_state.name AS state_name, 
 			products.name AS product_name, products.price, products.description, products.sales_volume, products.quantity, products.views,
 			products.is_deleted, products.wishlist_count, products.thumbnail_image, brands.brand_name
 			FROM tikkling 
