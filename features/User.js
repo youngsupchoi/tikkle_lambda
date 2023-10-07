e033de7d5f0edb2043bd5762d47dc796d1535308
youngsupchoi <sup1214@naver.com>
2023-10-07 23:36:20 +0900

    Feat: User 객체 생성

--- features/User.js
+++ features/User.js
@@ -1,3 +1,7 @@
+const { queryDatabase, queryDatabase_multi } = require("db.js");
+const { getSSMParameter } = require("ssm.js");
+const { ExpectedError} = require("./ExpectedError.js");
+
 class User {
   constructor({
     id,
@@ -35,5 +39,21 @@ class User {
     this.tikkling_ticket = tikkling_ticket;
     this.account = account;
     this.bank_code = bank_code;
+  }
+
+  createById = async (id) => {
+    try{
+      const query = `INSERT INTO users (id) VALUES (?);`;
+      const [user] = await queryDatabase(query, [id]);
+      return new User(user);
+
+    }catch(error){
+      console.error("error -> getUserById ");
+      throw new ExpectedError({
+        status: "500",
+        message: `서버에러`,
+        detail_code: "00",
+      });
+    }
   }
 }
