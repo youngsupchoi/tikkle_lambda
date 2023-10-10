42eea7d69191717762ad83be169847ed45cfe0fa
sjsjmine129 <sjsjmine129@gmail.com>
2023-10-10 13:08:44 +0900

    Add/Get_payment_data

--- tikkle_payment/post_payment_getData/index.js
+++ tikkle_payment/post_payment_getData/index.js
@@ -1,6 +1,7 @@
 const { Payment } = require("../../features/Payment");
 const { User } = require("../../features/User");
 const { Response } = require("../../features/Response");
+const axios = require("axios");
 
 exports.post_payment_getData = async (req, res) => {
 	const { body, id, returnToken } = req;
@@ -12,12 +13,35 @@ exports.post_payment_getData = async (req, res) => {
 		const Authorization = "Bearer " + token;
 
 		//-------- call port one AI for data --------------------------------------------------------------------------------------//
+		let result = null;
+
+		try {
+			const axios_result = await axios({
+				url: "https://api.iamport.kr/payments/" + merchant_uid,
+				method: "get",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: Authorization,
+				},
+			});
+
+			// console.log("RES : ", axios_result.data);
+			result = axios_result.data.response;
+		} catch (error) {
+			// Handle errors here
+			console.error("Error:", error);
+			throw new ExpectedError({
+				status: "500",
+				message: `서버에러`,
+				detail_code: "00",
+			});
+		}
 
 		//-------- return result --------------------------------------------------------------------------------------//
 
 		const return_body = {
 			success: true,
-			data: Authorization,
+			data: result,
 			detail_code: "00",
 			message: "success get product info",
 			returnToken: returnToken,
