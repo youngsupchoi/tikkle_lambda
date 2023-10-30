2e139afcefe8466a74b2bf8bf80c6e9c211bba23
youngsupchoi <sup1214@naver.com>
2023-10-30 11:20:56 +0900

    Fix #159: 쿼리에서 product.quantity제거

--- tikkle_tikkling/post_user_getTikklingDetail/index.js
+++ tikkle_tikkling/post_user_getTikklingDetail/index.js
@@ -15,7 +15,7 @@ exports.post_user_getTikklingDetail = async (req, res) => {
     const rows = await queryDatabase(
       `SELECT tikkling.id AS tikkling_id, tikkling.user_id, tikkling.resolution_type AS resolution_type, tikkling.type AS tikkling_type, tikkling.funding_limit, tikkling.created_at, tikkling.tikkle_quantity, tikkling.terminated_at, tikkling.product_id, tikkling.terminated_at,
 			tikkling.state_id, tikkling_state.name AS state_name, 
-			products.name AS product_name, products.price, products.description, products.sales_volume, products.quantity, products.views,
+			products.name AS product_name, products.price, products.description, products.sales_volume, products.views,
 			products.is_deleted, products.wishlist_count, products.thumbnail_image, brands.brand_name
 			FROM tikkling 
 			INNER JOIN tikkling_state ON tikkling.state_id = tikkling_state.id
--- tikkle_user/get_user_myWishlist/index.js
+++ tikkle_user/get_user_myWishlist/index.js
@@ -1,49 +1,49 @@
 const { queryDatabase } = require("db.js");
 
 exports.get_user_myWishlist = async (req, res) => {
-	const body = req.body;
-	const id = req.id;
-	const returnToken = req.returnToken;
+  const body = req.body;
+  const id = req.id;
+  const returnToken = req.returnToken;
 
-	//-------- check DB --------------------------------------------------------------------------------------//
+  //-------- check DB --------------------------------------------------------------------------------------//
 
-	let sqlResult;
+  let sqlResult;
 
-	try {
-		const rows = await queryDatabase(
-			`SELECT user_wish_list.product_id, user_wish_list.created_at, 
-			products.name, products.price, products.description, products.sales_volume, products.quantity, products.category_id, products.created_at, products.views,
+  try {
+    const rows = await queryDatabase(
+      `SELECT user_wish_list.product_id, user_wish_list.created_at, 
+			products.name, products.price, products.description, products.sales_volume, products.category_id, products.created_at, products.views,
 			products.is_deleted, products.wishlist_count, products.thumbnail_image, brands.brand_name
 				FROM user_wish_list
 				INNER JOIN products ON user_wish_list.product_id = products.id
 				INNER JOIN brands ON products.brand_id = brands.id
 				WHERE user_wish_list.user_id = ?
 				ORDER BY user_wish_list.created_at DESC`,
-			[id]
-		);
-		sqlResult = rows;
-		console.log("SQL result : ", sqlResult);
-	} catch (err) {
-		console.log(" get_user_myWishlist 에서 에러가 발생했습니다.", err);
-		const return_body = {
-			success: false,
-			detail_code: "00",
-			message: "SQL error",
-			returnToken: null,
-		};
-		return res.status(500).send(return_body);
-	}
+      [id]
+    );
+    sqlResult = rows;
+    console.log("SQL result : ", sqlResult);
+  } catch (err) {
+    console.log(" get_user_myWishlist 에서 에러가 발생했습니다.", err);
+    const return_body = {
+      success: false,
+      detail_code: "00",
+      message: "SQL error",
+      returnToken: null,
+    };
+    return res.status(500).send(return_body);
+  }
 
-	const retData = sqlResult;
+  const retData = sqlResult;
 
-	//-------- return result --------------------------------------------------------------------------------------//
+  //-------- return result --------------------------------------------------------------------------------------//
 
-	const return_body = {
-		success: true,
-		data: retData,
-		detail_code: "00",
-		message: "success get user myWishlist",
-		returnToken: returnToken,
-	};
-	return res.status(200).send(return_body);
+  const return_body = {
+    success: true,
+    data: retData,
+    detail_code: "00",
+    message: "success get user myWishlist",
+    returnToken: returnToken,
+  };
+  return res.status(200).send(return_body);
 };
