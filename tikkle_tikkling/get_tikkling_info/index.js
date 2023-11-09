8cf5deab655484ede516d3b328c5626704154b0d
sjsjmine129 <sjsjmine129@gmail.com>
2023-11-09 10:49:33 +0900

    fix

--- tikkle_tikkling/get_tikkling_info/index.js
+++ tikkle_tikkling/get_tikkling_info/index.js
@@ -28,9 +28,8 @@ exports.get_tikkling_info = async (req, res) => {
 			a.state_id,
 			a.option_combination_id,
       pc.name AS category_name 
-      p.id AS product_id,
+      a.product_id AS product_id,
       FROM active_tikkling_view a 
-      JOIN products p ON a.product_id = p.id
       JOIN users u ON a.user_id = u.id 
       JOIN product_category pc ON a.category_id = pc.id 
       WHERE u.id = ?;`;
@@ -85,9 +84,8 @@ exports.get_tikkling_info = async (req, res) => {
 			a.state_id,
 			a.option_combination_id,
       pc.name AS category_name 
-      p.id AS product_id,
+      a.product_id AS product_id,
       FROM active_tikkling_view a 
-      JOIN products p ON a.product_id = p.id
       JOIN users u ON a.user_id = u.id 
       JOIN product_category pc ON a.category_id = pc.id 
       WHERE a.tikkling_id = ? AND a.state_id = 1;
