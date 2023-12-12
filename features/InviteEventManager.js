e8784dc2dd9f66a2919072fbf9592ad447e1937f
youngsupchoi <sup1214@naver.com>
2023-12-12 17:57:57 +0900

    Feat #223: InviteEventManager 객체 생성

---
+++ features/InviteEventManager.js
@@ -0,0 +1,17 @@
+const { ExpectedError } = require("./ExpectedError.js");
+
+class InviteEventManager {
+  constructor() {}
+
+  //tikkle구매유저가 이전에 티클을 구매한 이력이 있는지 확인
+
+  //보너스 티클을 전송
+
+  //환급시 보너스 티클을 결제 취소 처리
+
+  //제공한 보너스 티클이 얼마인지 확인
+
+  //티클 환불시 보너스 티클 결제 취소처리
+}
+
+module.exports = { InviteEventManager };
