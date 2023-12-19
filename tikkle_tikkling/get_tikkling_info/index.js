211168bacd2791898900f6c51b467369ac22aa16
youngsupchoi <sup1214@naver.com>
2023-12-19 16:32:30 +0900

    Hotfix: 에러로그 수정

--- tikkle_tikkling/get_tikkling_info/index.js
+++ tikkle_tikkling/get_tikkling_info/index.js
@@ -147,7 +147,7 @@ exports.get_tikkling_info = async (req, res) => {
       };
       return res.status(400).send(return_body);
     } else {
-      console.error(`🚨 error -> ⚡️ post_tikkling_info : 🐞${error}`);
+      console.error(`🚨 error -> ⚡️ get_tikkling_info : 🐞${error}`);
       const return_body = {
         success: false,
         detail_code: "00",
